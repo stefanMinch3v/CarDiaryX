fa108512b9f8dd2c145a9bb2da1cd47257622c60
Stefan Minchev <sminchev@outlook.com>
2021-08-29 20:16:11 +0200

    #22 feat: add controller endpoints

--- src/CarDiaryX.Api/CarDiaryX.Web/Features/V1/TripsController.cs
+++ src/CarDiaryX.Api/CarDiaryX.Web/Features/V1/TripsController.cs
@@ -1,7 +1,10 @@
 ﻿using CarDiaryX.Application.Features.V1.Trips.Commands;
+using CarDiaryX.Application.Features.V1.Trips.OutputModels;
+using CarDiaryX.Application.Features.V1.Trips.Queries;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace CarDiaryX.Web.Features.V1
@@ -11,19 +14,23 @@ namespace CarDiaryX.Web.Features.V1
     public class TripsController : ApiControllerBase
     {
         [HttpPost]
-        public async Task<IActionResult> Add(AddNewTripCommand command)
+        public async Task<ActionResult> Add(AddNewTripCommand command)
             => await base.Send(command);
 
-        //[HttpGet]
-        //public async Task<IActionResult> Get()
-        //    => base.Send(null);
+        [HttpGet]
+        public async Task<ActionResult<TripDetailsOutputModel>> Get(GetTripQuery query)
+            => await base.Send(query);
 
-        //[HttpDelete]
-        //public async Task<IActionResult> Delete()
-        //    => base.Send(null);
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyCollection<TripListOutputModel>>> GetAll(GetAllTripsQuery query)
+            => await base.Send(query);
 
-        //[HttpPut]
-        //public async Task<IActionResult> Update()
-        //    => base.Send(null);
+        [HttpDelete]
+        public async Task<ActionResult> Delete(DeleteTripCommand command)
+            => await base.Send(command);
+
+        [HttpPut]
+        public async Task<ActionResult> Update(UpdateTripCommand command)
+            => await base.Send(command);
     }
 }
